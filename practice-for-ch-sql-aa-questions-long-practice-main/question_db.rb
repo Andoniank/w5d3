26d57cb4e0ba73daa22317acc6be9a64c3c4bdb5
Raymond <raymondtan676@gmail.com>
2023-08-16 17:26:12 -0700

    Work on class replies and questions

--- practice-for-ch-sql-aa-questions-long-practice-main/question_db.rb
+++ practice-for-ch-sql-aa-questions-long-practice-main/question_db.rb
@@ -1,5 +1,6 @@
 require 'singleton'
 require 'sqlite3'
+require 'byebug'
 
 class QuestionDatabase < SQLite3::Database
     include Singleton
@@ -57,20 +58,22 @@ class User
     def authored_questions 
         author = Question.find_by_author_id(self.id)
         raise "#{self} not found in DB" unless author
+        author
     end
 
     def authored_replies
         author = Reply.find_by_user_id(self.id)
         raise "#{self} not found in DB" unless author
+        author
     end
 
     def create
         raise "#{self} already in database" if self.id
         QuestionDatabase.instance.execute(<<-SQL, self.fname, self.lname)
-          INSERT INTO
-            users (fname, lname)
-          VALUES
-            (?, ?)
+            INSERT INTO
+                users (fname, lname)
+            VALUES
+                (?, ?)
         SQL
         self.id = QuestionDatabase.instance.last_insert_row_id
     end
@@ -78,12 +81,12 @@ class User
     def update
         raise "#{self} not in database" unless self.id
         QuestionDatabase.instance.execute(<<-SQL, self.fname, self.lname, self.id)
-          UPDATE
-            users
-          SET
-            fname = ?, lname = ?
-          WHERE
-            id = ?
+            UPDATE
+                users
+            SET
+                fname = ?, lname = ?
+            WHERE
+                id = ?
         SQL
     end
 end
@@ -91,7 +94,8 @@ end
 #--------------------------------------------------------
 
 class Question
-    
+    attr_accessor :id, :title, :body, :author_id
+
     def self.all
         data = QuestionDatabase.instance.execute("SELECT * FROM questions")
         data.map {|datum| Question.new(datum)}
@@ -110,7 +114,7 @@ class Question
         Question.new(q_id.first)
     end
 
-    def find_by_author_id(author_id)
+    def self.find_by_author_id(author_id)
         question = QuestionDatabase.instance.execute(<<-SQL, author_id)
             SELECT
                 *
@@ -123,10 +127,32 @@ class Question
         Question.new(question.first)
     end
 
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @author_id = options['author_id']
+    end
+
     def author
+        question = QuestionDatabase.instance.execute(<<-SQL, author_id)
+            SELECT
+                fname, lname
+            FROM 
+                questions
+            JOIN
+                users ON author_id = users.id
+            WHERE
+                author_id = ?
+            SQL
+        return nil if question.length == 0
+        Question.new(question.first)
     end
 
     def replies
+        reply = Reply.find_by_question_id(self.id)
+        raise "#{self} not found in DB" unless reply
+        reply
     end
 
 
@@ -143,9 +169,19 @@ end
 
 class Reply
     def self.find_by_id
+        reply_id = QuestionDatabase.instance.execute(<<-SQL, id)
+        SELECT
+            *
+        FROM 
+            replies
+        WHERE
+            id = ?
+        SQL
+        return nil if reply_id.length == 0
+        Question.new(reply_id.first)
     end
 
-    def find_by_user_id(user_id)
+    def self.find_by_user_id(user_id)
         reply = QuestionDatabase.instance.execute(<<-SQL, user_id)
             SELECT
                 *
@@ -158,10 +194,32 @@ class Reply
         Reply.new(reply.first)
     end
 
-    def find_by_question_id(question_id)
+    def self.find_by_question_id(question_id)
+        reply = QuestionDatabase.instance.execute(<<-SQL, question_id)
+        SELECT
+            *
+        FROM 
+            replies
+        WHERE
+            question_id = ?
+    SQL
+    return nil if reply.length == 0
+    Reply.new(reply.first)
     end
 
     def author
+        reply = QuestionDatabase.instance.execute(<<-SQL, author_id)
+            SELECT
+                *
+            FROM 
+                replies
+            JOIN 
+                
+            WHERE
+                author_id = ?
+        SQL
+        return nil if question.length == 0
+        Question.new(question.first)
     end
 
     def question
